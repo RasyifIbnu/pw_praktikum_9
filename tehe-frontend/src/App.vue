9c1541e57f3227992a6dd492258d24aa4a7bf119
RasyifIbnu <rasyifibnu23@gmail.com>
2024-06-12 22:16:27 +0700

    praktikum 9

---
+++ tehe-frontend/src/App.vue
@@ -0,0 +1,31 @@
+
+
+<template>
+
+
+    <div id="app">
+ <nav>
+ <ul>
+ <li><router-link to="/">Home</router-link></li>
+ <li><router-link to="/about">about</router-link></li>
+ <li><router-link to="/service">services</router-link></li>
+ </ul>
+ </nav>
+ <router-view/>
+ </div>
+</template>
+<script>
+export default {
+ name: 'App'
+}
+</script>
+<style>
+nav ul {
+ list-style-type: none;
+ padding: 0;
+}
+nav ul li {
+ display: inline;
+ margin-right: 10px;
+}
+</style>
